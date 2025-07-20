91e94f9df23545de24f7a484552a28aef7b5e150
Plat-26 <lolade.badmus97@gmail.com>
2025-07-20 21:06:27 +0200

    Update log dependencies

---
+++ src/main/java/com/plat/app/chunks/ChunkHeader.java
@@ -0,0 +1,33 @@
+package com.plat.app.chunks;
+
+import com.plat.app.handlers.ServerIO;
+
+import java.io.IOException;
+
+public class BasicHeader {
+
+    public int csid;
+    public int fmt;
+
+    public BasicHeader(int fmt, int csid,) {
+        this.fmt = fmt;
+        this.csid = csid;
+    }
+
+    public static BasicHeader read(ServerIO io) throws IOException {
+        int b = io.readByte() & 0xFF;
+        int fmt = (b >>> 6) & 0x03;
+        int csid = b & 0x3F;
+
+        if (csid == 0) {
+            // 2nd byte + 64
+            csid = (io.readByte() & 0xFF) + 64;
+        } else if (csid == 1) {
+            // third byte*256 + second byte + 64
+            int lo = io.readByte() & 0xFF;
+            int hi = io.readByte() & 0xFF;
+            csid =  64 + lo + (hi << 8);
+        }
+        return new BasicHeader(fmt, csid);
+    }
+}
---
+++ src/main/java/com/plat/app/chunks/MsgHeader.java
@@ -0,0 +1,2 @@
+package com.plat.app.chunks;public class MsgHeader {
+}
